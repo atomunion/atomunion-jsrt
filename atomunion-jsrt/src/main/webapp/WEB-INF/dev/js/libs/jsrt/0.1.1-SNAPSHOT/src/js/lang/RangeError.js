c497b17795b651d64e43cc31db93280028251a73
liujc <liujc@Ctrip.com>
2014-06-27 16:52:25 +0800

    fixed

---
+++ atomunion-jsrt/src/main/webapp/WEB-INF/dev/js/libs/jsrt/0.1.1-SNAPSHOT/src/js/lang/RangeError.js
@@ -0,0 +1,23 @@
+/*!
+ * JSRT JavaScript Library 0.2.1
+ * lico.atom@gmail.com
+ *
+ * Copyright 2008, 2014 Atom Union, Inc.
+ * Released under the MIT license
+ *
+ * Date: 2014年6月25日
+ */
+
+$import("js.lang.Throwable");
+Class.forName({
+	name : "class RangeError",
+	alias:"js.lang.RangeError",
+	
+	"private name" : "js.lang.RangeError",// 错误名
+	"private number" : 3,
+	
+	RangeError : function() {
+	}
+});
+Object.extend(RangeError, js.lang.Throwable.$class.getMethods(),
+        'prototype', '_value');
